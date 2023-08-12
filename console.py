f709419eb2c1e67264ef4d3f899f169e2ad6586d
catherinekimani <catherinekimani882@gmail.com>
2023-08-12 10:24:26 +0300

    Extend console.py for new class commands

--- console.py
+++ console.py
@@ -5,13 +5,27 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """ command interpreter class """
     prompt = "(hbnb) "
-    classes = {"BaseModel": BaseModel, "User": User}
+    classes = {
+                "BaseModel": BaseModel,
+                "User": User,
+                "Place": Place,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Review": Review
+                }
 
     def emptyline(self):
         """ Do nothing on empty input line """
